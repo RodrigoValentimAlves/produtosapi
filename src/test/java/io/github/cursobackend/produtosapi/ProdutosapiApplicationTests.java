fd80c1d86597ef49c499cd65efc854b13fc9db3d
Rodrigo Valentim <rodrigo.valentim.alves@outlook.com>
2025-06-17 17:02:21 -0300

    feat: criando projeto

---
+++ src/test/java/io/github/cursobackend/produtosapi/ProdutosapiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.cursobackend.produtosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
