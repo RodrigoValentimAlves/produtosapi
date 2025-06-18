e9680ad9486bcb09a8cc54d5de8c302c7b1280a2
Rodrigo Valentim <rodrigo.valentim.alves@outlook.com>
2025-06-18 17:46:26 -0300

    feat: adiciona endpoint de busca por id

--- src/main/java/io/github/cursobackend/produtosapi/controller/ProdutoController.java
+++ src/main/java/io/github/cursobackend/produtosapi/controller/ProdutoController.java
@@ -2,11 +2,9 @@ package io.github.cursobackend.produtosapi.controller;
 
 import io.github.cursobackend.produtosapi.model.Produto;
 import io.github.cursobackend.produtosapi.repository.ProdutoRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -33,5 +31,20 @@ public class ProdutoController {
 
         produtoRepository.save(produto);
         return produto;
+    }
+
+    /**
+     * Recupera um produto pelo seu identificador único.
+     *
+     * Este endpoint realiza uma busca no repositório de produtos com base no ID fornecido
+     * na URL. Caso o produto seja encontrado, ele é retornado no corpo da resposta; caso
+     * contrário, retorna {@code null}.
+     *
+     * @param id o identificador único do produto a ser recuperado
+     * @return o objeto {@link Produto} correspondente ao ID informado, ou {@code null} se não encontrado
+     */
+    @GetMapping("/buscarPorId/{id}")
+    public Produto obterPorId(@PathVariable("id") String id) {
+        return produtoRepository.findById(id).orElse(null);
     }
 }
